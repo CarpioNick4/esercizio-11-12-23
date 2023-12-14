472967798882ea68ea3e44d978a35076f6aa6807
CarpioNick4 <carpio.nick@iisgalvanimi.edu.it>
2023-12-14 08:58:30 +0000

    non finito

--- html_statici/app.js
+++ html_statici/app.js
@@ -1,14 +1,17 @@
 var express = require('express');
+const path = require('path');
 var app = express(); 
-app.get('/', function(req,res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
-   });   
+  
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
-app.get('/calciatori',function(req,res){
-    res.sendFile(path.join(__dirname,'calciatori.html')); //__dirname : Ritorna la cartella del progetto
+
+app.get('/', function(req,res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+   }); 
+app.get('/r_Madrid',function(req,res){
+    res.sendFile(path.join(__dirname,'c_real.html')); //__dirname : Ritorna la cartella del progetto
 });
-app.get('/cr7',function(req,res){
-    res.sendFile(path.join(__dirname,'cr7.html')); //__dirname : Ritorna la cartella del progetto
+app.get('/barca',function(req,res){
+    res.sendFile(path.join(__dirname,'c_barca.html')); //__dirname : Ritorna la cartella del progetto
 });
